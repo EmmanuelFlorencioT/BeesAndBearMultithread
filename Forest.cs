08003d06ea86e2d8aff0eecae14ded5774fd0938
EmmanuelFlorencioT <emm.ftr2@gmail.com>
2022-10-04 09:22:25 -0500

    Added the code of the project

---
+++ Forest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace BearAndBeesMultithread
+{
+    internal class Forest
+    {
+        private int jarCap, jar, turn = 0, counter = 0, threadExit = 0;
+        private bool bearAwake;
+        private int[] beesCap = new int[10];
+
+        public Forest(int N)
+        {
+            jarCap = N;
+            jar = 0;
+            createBees();
+            createBear();
+        }
+
+        private void createBees()
+        {
+            Random r = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                beesCap[i] = r.Next(1, jarCap);
+                Thread bee = new Thread(fillJar);
+                bee.Start(i);
+            }
+        }
+        
+        private void createBear()
+        {
+            bearAwake = false;
+            Thread bear = new Thread(takeJar);
+            bear.Start();
+        }
+        
+        private void fillJar(Object obj)
+        {
+            int tid = (int)obj;
+
+            while(counter < 5)
+            {
+                if(turn == tid && bearAwake == false)
+                {
+                    Console.WriteLine("Bee {0} deposits {1} units of honey into the jar.", tid, beesCap[tid]);
+                    jar += beesCap[tid];/*
+                    if ((jar += beesCap[tid]) >= jarCap)
+                    {
+                        Console.WriteLine("Jar has now {0} units of honey.\n", jar);
+                        bearAwake = true;
+                    }*/
+                    Console.WriteLine("Jar has now {0} units of honey.\n", jar);
+                    if(jar >= jarCap)
+                    {
+                        bearAwake = true;
+                    }
+                    turn = (turn + 1) % 10;
+                }
+            }
+            threadExit++;
+        }
+
+        private void takeJar()
+        {
+            while(threadExit < 10)
+            {
+                if(bearAwake == true)
+                {
+                    Console.WriteLine("--Bear ate {0} units of honey\n", jar);
+                    jar = 0;
+                    counter++;
+                    bearAwake = false;
+                }
+            }
+            threadExit++;
+        }
+
+        public bool checkThreadExit()
+        {
+            return threadExit < 11;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearAndBeesMultithread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest(20);
+
+            while (f.checkThreadExit()) { }
+
+            Console.WriteLine("Program finished");
+        }
+    }
+}
